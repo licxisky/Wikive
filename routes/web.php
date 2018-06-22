e0768f64a9bec17b06642c831c312d34ed9b9c02
LiCxi <licxisky@qq.com>
2018-06-22 23:00:13 +0800

    Add admin.projects.index route

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/documents/{document}', 'DocumentController@show')->name('documents.show');
+
+Route::group(['prefix' => '/admin'], function () {
+    Route::get('/project', 'Admin\ProjectController@index')->name('admin.projects.index');
+});
