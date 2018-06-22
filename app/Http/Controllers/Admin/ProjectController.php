ecb3ee63c7e45b7e5f3dd53d9043be19ec1b14c3
LiCxi <licxisky@qq.com>
2018-06-23 02:35:06 +0800

    Add Project resource method

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
@@ -16,5 +18,28 @@ class ProjectController extends Controller
     public  function index() {
         $projects = Project::with('user')->get();
         return view('admin.projects.index', compact('projects'));
+    }
+
+    public function create() {
+        return view('admin.projects.create');
+    }
+
+    public function store(ProjectRequest $projectRequest, Project $project){
+        Auth::user()->projects()->create($projectRequest->all());
+        return redirect()->route('admin.projects.index');
+    }
+
+    public function edit(Project $project) {
+        return view('admin.projects.edit', compact('project'));
+    }
+
+    public function update(ProjectRequest $projectRequest, Project $project) {
+        $project->update($projectRequest->all());
+        return redirect()->route('admin.projects.index');
+    }
+
+    public function destroy(Project $project) {
+        $project->delete();
+        return back();
     }
 }
